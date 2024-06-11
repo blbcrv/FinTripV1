c6d4b452cfe3b4aa363d9e7b7cb2cf2ee4c5a859
Clément <clementpages02@gmail.com>
2024-06-11 14:18:49 +0200

    Initial commit

---
+++ FinTrip-Frontend/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const cors = require('cors');
+const path = require('path');
+const logger = require('../FinTrip-Backend/logger/logger');
+
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, './src/assets')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+    logger.info(`Le frontend est démarré sur le port ${port}`, { msName: 'Fintrip-Frontend' });
+})
